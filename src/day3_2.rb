4a213ad0e2f7a65a3c3948b6b56971d659780bab
jasperdewinther <31817802+jasperdewinther@users.noreply.github.com>
2021-12-03 23:34:46 +0100

    finished day 3

--- src/day3_2.rb
+++ src/day3_2.rb
@@ -6,30 +6,20 @@ def run(input)
   oxy = 0
   co2 = 0
 
-
   input[0].length.times do |i|
     one_counter_oxy = filtered_oxy.count { |c| c[i] == '1' }
-    common_oxy = one_counter_oxy > filtered_oxy.length / 2
-
-    filtered_oxy = filtered_oxy.select { |v| one_counter_oxy != filtered_oxy.length / 2 ? common_oxy == !v[i].to_i.zero? : !v[i].to_i.zero? }
-
+    common_oxy = one_counter_oxy >= filtered_oxy.length.to_f / 2.0
+    filtered_oxy = filtered_oxy.select { |v| !common_oxy == v[i].to_i.zero? }
     oxy = filtered_oxy[0].to_i(2)
     break if filtered_oxy.length == 1
   end
 
-
   input[0].length.times do |i|
     one_counter_co2 = filtered_co2.count { |c| c[i] == '1' }
-    common_co2 = one_counter_co2 > filtered_co2.length / 2
-
-    filtered_co2 = filtered_co2.select { |v| one_counter_co2 != filtered_co2.length / 2 ? !common_co2 == !v[i].to_i.zero? :  v[i].to_i.zero?}
-
+    common_co2 = one_counter_co2 >= filtered_co2.length.to_f / 2.0
+    filtered_co2 = filtered_co2.select { |v| common_co2 == v[i].to_i.zero?}
     co2 = filtered_co2[0].to_i(2)
     break if filtered_co2.length == 1
   end
-  print oxy
-  print "\n"
-  print co2
-  print "\n"
   oxy * co2
 end
